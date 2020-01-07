b70940ae78ff19c2bf870515933722e24a8b9e5c
Porrello Alexander <PORRELLO@kapsch.net>
2020-01-07 08:55:16 -0600

    Code refactor.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { DataReaderService } from './data-reader.service';
-import { Versions } from './models/versions';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'opendap-angular';
+  title = 'OPeNDAP™';
   constructor() { }  
 }
