9c7240795134a625304ce1454f099d2d0b35af5c
Porrello Alexander <PORRELLO@kapsch.net>
2020-01-07 14:02:36 -0600

    Nav component complete.

--- src/app/components/navigation-dock/navigation-dock.component.ts
+++ src/app/components/navigation-dock/navigation-dock.component.ts
@@ -9,18 +9,18 @@ export class NavigationDockComponent implements OnInit {
   @Input() ids: any[];
   @Input() scrollOffset = 75;
 
-  activeLink: any;
+  activeSection: any;
 
   constructor() { }
 
   ngOnInit(): void {
-    this.activeLink = this.ids[0];
+    this.activeSection = this.ids[0];
 
     window.addEventListener('scroll', () => {
       for (let i = 0; i < this.ids.length; i++) {
         if (document.getElementById(this.ids[i].id) !== null) {
           if (this.ids[i + 1] === undefined) {
-            this.activeLink = this.ids[i];
+            this.activeSection = this.ids[i];
             break;
           }
 
@@ -28,7 +28,7 @@ export class NavigationDockComponent implements OnInit {
           let b = window.scrollY + this.scrollOffset + 15 < document.getElementById(this.ids[i + 1].id).offsetTop;
 
           if (a && b) {
-            this.activeLink = this.ids[i];
+            this.activeSection = this.ids[i];
             break;
           }
         }
@@ -37,8 +37,7 @@ export class NavigationDockComponent implements OnInit {
   }
 
   scrollIntoView(link: any, id: string) {
-    this.activeLink = link;
-
+    this.activeSection = link;
     window.scrollTo({
       top: document.getElementById(id).offsetTop - this.scrollOffset,
       behavior: "smooth"
