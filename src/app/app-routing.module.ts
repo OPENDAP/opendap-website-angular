f803140d449775384436d8585edb139e1189a57e
Porrello Alexander <PORRELLO@kapsch.net>
2020-01-09 14:22:14 -0600

    Refactored project.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ReleasesPageComponent } from './components/releases-page/releases-page.component';
-import { AboutUsComponent } from './components/about-us/about-us.component';
+import { AboutUsComponent } from './components/content-heavy/about-us/about-us.component';
 import { HyraxComponent } from './components/hyrax/hyrax.component';
 import { BoilerplateMakerComponent } from './components/boilerplate-maker/boilerplate-maker.component';
 import { BoilerplateEditorComponent } from './components/boilerplate-maker/boilerplate-editor/boilerplate-editor.component';
@@ -9,8 +9,8 @@ import { HomeComponent } from './components/home/home.component';
 import { FaqComponent } from './components/faq/faq.component';
 import { SupportComponent } from './components/support/support.component';
 import { FaqSingleSectionComponent } from './components/faq/faq-single-section/faq-single-section.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-import { SoftwareComponent } from './components/software/software.component';
+import { NotFoundComponent } from './components/navigation/not-found/not-found.component';
+import { SoftwareComponent } from './components/content-heavy/software/software.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,21 +28,21 @@ import { ReleasesPageComponent } from './components/releases-page/releases-page.
 
 import { HttpClientModule } from '@angular/common/http';
 import { BoilerplateComponent } from './components/boilerplate/boilerplate.component';
-import { AboutUsComponent } from './components/about-us/about-us.component';
+import { AboutUsComponent } from './components/content-heavy/about-us/about-us.component';
 import { HyraxComponent } from './components/hyrax/hyrax.component';
 import { BoilerplateMakerComponent } from './components/boilerplate-maker/boilerplate-maker.component';
 import { BoilerplateEditorComponent } from './components/boilerplate-maker/boilerplate-editor/boilerplate-editor.component';
 import { HomeComponent } from './components/home/home.component';
-import { NavComponent } from './components/nav/nav.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/navigation/nav/nav.component';
+import { FooterComponent } from './components/navigation/footer/footer.component';
 import { FaqComponent } from './components/faq/faq.component';
 import { FaqSectionComponent } from './components/faq/faq-section/faq-section.component';
 import { FaqSearchComponent } from './components/faq/faq-search/faq-search.component';
 import { SupportComponent } from './components/support/support.component';
-import { NavigationDockComponent } from './components/navigation-dock/navigation-dock.component';
+import { NavigationDockComponent } from './components/navigation/navigation-dock/navigation-dock.component';
 import { FaqSingleSectionComponent } from './components/faq/faq-single-section/faq-single-section.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-import { SoftwareComponent } from './components/software/software.component';
+import { NotFoundComponent } from './components/navigation/not-found/not-found.component';
+import { SoftwareComponent } from './components/content-heavy/software/software.component';
 import { MarkdownComponent } from './components/markdown/markdown.component';
 
 @NgModule({
